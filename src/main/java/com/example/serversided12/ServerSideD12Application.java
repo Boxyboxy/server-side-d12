e06748a6cc0b3bd0440ca05c73f2a3c257ab8c55
boxyboxy <eugenengboxiang@gmail.com>
2022-01-06 09:37:06 +0800

    init Spring initiliasr

---
+++ src/main/java/com/example/serversided12/ServerSideD12Application.java
@@ -0,0 +1,13 @@
+package com.example.serversided12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideD12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideD12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serversided12/ServerSideD12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serversided12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideD12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
